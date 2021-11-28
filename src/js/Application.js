b7ba17873fabf57786c35f852609dbea4e931e43
Kristiqn <kristiqn_estatiev@abv.bg>
2021-11-28 12:42:24 +0200

    fix: _beat to be class App instance

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,11 +11,29 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin` alive", "stayin` alive"];
+    let count = 0;
+
+    this._beat = new Beat();
+
+    this._beat.addListener(Beat.events.BIT, () => {
+      if (count < lyrics.length) {
+        document
+          .querySelector(".main")
+          .appendChild(this._create(lyrics[count]));
+      } else {
+        this._beat.removeListener(Beat.events.BIT);
+      }
+      count++;
     });
 
     this.emit(Application.events.READY);
+  }
+
+  _create(lyricText) {
+    const message = document.createElement("div");
+    message.classList.add("message");
+    message.innerText = lyricText;
+    return message;
   }
 }
