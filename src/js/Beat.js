5ad501b090373a52fd163f2b7576b752f7e6c2b6
Kristiqn <kristiqn_estatiev@abv.bg>
2021-11-28 12:43:29 +0200

    feat: Create a ".message" for each lyric

---
+++ src/js/Beat.js
@@ -0,0 +1,16 @@
+import EventEmitter from "eventemitter3";
+
+export default class Beat extends EventEmitter {
+  static get events() {
+    return {
+      BIT: "bit",
+    };
+  }
+
+  constructor() {
+    super();
+    setInterval(() => {
+      this.emit(Beat.events.BIT);
+    }, 600);
+  }
+}
